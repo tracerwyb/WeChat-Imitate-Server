bf1d06e7fc6cdd62e35264bf0ba6b080924dc79c
2021051615301Zhangjing <3326336521@qq.com>
2024-05-17 22:42:20 +0800

    write json data with id and date

--- MessageBroker.cpp
+++
@@ -1,9 +0,0 @@
-#include "MessageBroker.h"
-
-MessageBroker *MessageBroker::getInstance() {}
-
-bool MessageBroker::addNewMessage() {}
-
-bool MessageBroker::delMsgForUser() {}
-
-bool MessageBroker::deleteMessage() {}
--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include "network.h"
 #include "message.h"
-#include "server.h"
 #include <QDebug>
 #include <sys/ioctl.h>
-
+#include <nlohmann/json.hpp>
+#include <iostream>
 int main(int argc, char *argv[])
 {
     Network network;
     network.createSocket();//
-    Server server;
 
     int cnnfd=network.acceptSocket();
     int cnnfd2=network.acceptSocket();
@@ -19,15 +18,15 @@ int main(int argc, char *argv[])
             qDebug()<<"zheshijieguo  cnnfd:"<<result;
             network.recieveMessage(cnnfd,buf);
             network.sendMessage(cnnfd2,buf);
-            server.conversionJson(buf);  //this conversion turn json_string to json object,infact just a row,when you understand please change or delete it.
+            auto j=nlohmann::json::parse(buf);
+            std::cout<<"jso bj:"<<j.at("id")<<std::endl;
         }
         if(int re=network.Select(cnnfd2)){
             qDebug()<<"zheshijieguo  cnnfd2:"<<re;
             network.recieveMessage(cnnfd2,buf);
             network.sendMessage(cnnfd,buf);
-            server.conversionJson(buf);
-
+            auto j=nlohmann::json::parse(buf);
+            std::cout<<"jso bj:"<<j.at("id")<<std::endl;
         }
     }
-
 }
